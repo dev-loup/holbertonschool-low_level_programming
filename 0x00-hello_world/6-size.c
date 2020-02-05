0867adf494cf36611ea4d813b5f5d8504bf3dc24
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-05 20:38:27 +0000

    6 activity size of characters added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char : %lu bytes\n", sizeof(char));
+printf("Size of an int : %lu bytes\n", sizeof(int));
+printf("Size of an long int : %lu bytes\n", sizeof(long));
+printf("Size of an long long int : %lu bytes\n", sizeof(long long));
+printf("Size of an float : %lu bytes\n", sizeof(float));
+return (0);
+}
