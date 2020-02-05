10c02eb3a6cd2fbb146d58c668b519b6de8617d7
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-05 18:27:19 +0000

    string printer

--- 0x00-hello_world/4-puts.s
+++ 0x00-hello_world/4-puts.s
@@ -1,4 +1,8 @@
-	.file	"main.c"
+	.file	"4-puts.c"
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Programming is like building a multilingual puzzle"
 	.text
 	.globl	main
 	.type	main, @function
@@ -10,7 +14,8 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	movl	$0, %eax
+	movl	$.LC0, %edi
+	call	puts
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
