59d08c3b7076441e9143b427be96c2a8e40a3f28
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-06 21:56:59 +0000

    alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+do {
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+c++;
+}
+else
+c++;
+} while (c <= 'z');
+putchar('\n');
+return (0);
+}
