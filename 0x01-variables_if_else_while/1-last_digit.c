b0072fbd0da7a1a05a28ede94d6b95b8fc9bbb2e
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-06 20:23:41 +0000

    fixed constant order first the variable next the comparison

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int u;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 u = n % 10;
-if (6 > u && u != 0)
+if (u < 6 && u != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, u);
 else if (u == 0)
 printf("Last digit of %d is %d and is 0\n", n, u);
