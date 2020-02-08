b5ec3276c6b392443dbe758c95cf325fab45e07a
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-08 02:42:50 +0000

    12 activity with one variable successed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,16 +12,16 @@ int main(void)
 int i;
 for (i = 0; i < 100 ; i++)
 {
-if ((i / 10) < (i % 10))
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-}
-if (i < 89 && (i / 10) < (i % 10))
-{
-putchar(',');
-putchar(' ');
-}
+	if ((i / 10) < (i % 10))
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+	}
+	if (i < 89 && (i / 10) < (i % 10))
+	{
+		putchar(',');
+		putchar(' ');
+	}
 }
 putchar('\n');
 return (0);
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 1000 ; i++)
+{
+	if ((i % 10) > ((i / 10) % 10) && (i % 10) != ((i / 10) % 10))
+		if ((i % 10) > (i / 100) && (i % 10) != (i / 100))
+		{
+			putchar((i / 100) + '0');
+			putchar(((i / 10) % 10) + '0');
+			putchar((i % 10) + '0');
+		if (i < 999)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		else
+		putchar('\n');
+		}
+}
+return (0);
+}
