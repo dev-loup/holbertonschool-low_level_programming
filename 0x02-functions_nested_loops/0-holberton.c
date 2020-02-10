2dbc59b7a6bc8a5c1afb56e1fa207c2bd0b5ca08
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-10 23:22:15 +0000

    holberton program done with library

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'H';
+	_putchar(c);
+	c = 'o';
+	_putchar(c);
+        c = 'l';
+        _putchar(c);
+	c = 'b';
+        _putchar(c);
+        c = 'e';
+	_putchar(c);
+	c = 'r';
+        _putchar(c);
+        c = 't';
+	_putchar(c);
+        c = 'o';
+	_putchar(c);
+        c = 'n';
+        _putchar(c);
+	c = '\n';
+	_putchar(c);
+return (0);
+}
