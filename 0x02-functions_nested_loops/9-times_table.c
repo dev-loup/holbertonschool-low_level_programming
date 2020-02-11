8c700fadf7ce36a0121c4367e37f49008b3a9c83
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-11 21:23:06 +0000

    times table done with spaces

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the table from 0 to 9
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void times_table(void)
+{
+	int n;
+	int m;
+	int r;
+
+	for (m = 0; m <= 9; m++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+		r = (n * m);
+		if (r <= 9)
+		{
+			_putchar(' ');
+			_putchar(r + '0');
+		}
+		else
+		{
+			_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
+		}
+
+		if (n < 9)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+		else
+			_putchar('\n');
+		}
+	}
+}
