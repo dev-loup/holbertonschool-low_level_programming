d656590cdd1f398404310f2572eac6100b9459d8
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-11 21:53:45 +0000

    addition performed with 10-main.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - prints the sum between two values
+ * @a: value 1
+ * @b: value 2
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+	int r;
+
+		r = (a + b);
+	return (r);
+}
