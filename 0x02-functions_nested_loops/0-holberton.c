b502d69b0ca2ccb75051a68ac1eeb42b4bd0da48
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-11 15:23:37 +0000

    0 holberton fixed

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,33 +8,36 @@
 int main(void)
 {
 	char c = 'H';
+
 	do {
 	_putchar(c);
-	if(c == 'H')
+	if (c == 'H')
 		c = 'o';
-	else if(c == 'o')
+	else if (c == 'o')
 		c = 'l';
-	else if(c == 'l')
+	else if (c == 'l')
 		c = 'b';
-	else if(c == 'b')
+	else if (c == 'b')
 		c = 'e';
-	else if(c == 'e')
+	else if (c == 'e')
 		c = 'r';
-	else if(c == 'r')
+	else if (c == 'r')
 		c = 't';
-	else if(c == 't')
+	else if (c == 't')
 		c = 'z';
-	} while(c != 'z');
+	} while (c != 'z');
+
 	c = 'o';
+
 	do {
 		_putchar(c);
-	if(c == 'o')
+	if (c == 'o')
 		c = 'n';
-	else if(c == 'n')
+	else if (c == 'n')
 		c = '\n';
-	else if(c == '\n')
+	else if (c == '\n')
 		c = 'z';
-	} while(c != 'z');
+	} while (c != 'z');
 
 return (0);
 }
