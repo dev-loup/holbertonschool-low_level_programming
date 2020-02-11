24fd268ab896faf87495dc10859fc69db76b9688
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-11 13:59:35 +0000

    alphabet for checker

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
