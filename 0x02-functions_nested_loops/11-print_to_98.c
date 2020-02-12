0fef9c03a52d5f874e387f18433ef633698fc766
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-12 16:52:55 +0000

    11 activity fixed loops and ASCII adition in character values

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,91 +1,67 @@
 #include "holberton.h"
 
 /**
- * print_to_98 - prints the values between n value and 98
- * @n: the value
+ * print_to_98 - prints the absolute sequence to 98
+ *
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void print_to_98(int n)
 {
-
 	do {
-		int m;
-
-		if (n > 99 || n < -99)
+		if (n > 98)
 		{
 			if (n > 99)
 			{
 				_putchar((n / 100) + '0');
-				_putchar(((n / 10) % 10) + '0');
+				_putchar(((n /10) % 10) + '0');
 				_putchar((n % 10) + '0');
-				if (n != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				n--;
+			       	n--;
 			}
 			else
 			{
-				m = n;
-				n = (m * -1);
-				_putchar('-');
-				_putchar((n / 100) + '0');
-				_putchar(((n / 10) % 10) + '0');
+				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
 				n--;
-				m = n;
-				n = (m * -1);
 			}
 		}
-		else if (n > 9 || n < -9)
+		if (n < 98)
 		{
-			if (n > 0)
+			if (n > 9)
 			{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			n++;
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				n++;
 			}
-			else
+			else if (n <= 9 && n >= 0)
 			{
-				m = n;
-				n = (m * -1);
+				_putchar((n % 10) + '0');
+				n++;
+			}
+			else if (n < 0 && n > -10)
+			{
+				n = (n * -1);
+				_putchar('-');
+				_putchar((n % 10) + '0');
+				_putchar(' ');
+				n--;
+				n = (n * -1);
+			}
+			else if (n <= -9 && n > -100)
+			{
+				n = (n* -1);
 				_putchar('-');
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
 				n--;
-				m = n;
-				n = (m * -1);
+				n = (n * -1);
 			}
 		}
-		else if (n >= -9 && n <= -1)
-		{
-			m = n;
-			n = (m * -1);
-			_putchar('-');
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
-			n--;
-			m = n;
-			n = (m * -1);
-		}
-		else
-		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
-			n++;
-		}
-
-	} while (n <= 98);
+		_putchar(',');
+		_putchar(' ');
+	} while (n != 98);
+	_putchar((n/10) + '0');
+	_putchar((n % 10) + '0');
 	_putchar('\n');
 }
