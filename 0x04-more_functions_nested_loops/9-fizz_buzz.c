5501fc29052836a62ec7f14e6c9a65b5b6c73ca7
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-13 22:54:07 +0000

    fizz buzz code ternary

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz multiples of three Buzz multiples of five
+ *
+ *
+ * Return: On success 0.
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		((a % 3) == 0) ?
+		  (((a % 5) == 0) ? (printf("FizzBuzz ")) : (printf("Fizz "))) :
+		  (((a % 5) == 0) ? (printf("Buzz ")) : (printf("%i ", a)));
+
+	}
+	return (0);
+}
