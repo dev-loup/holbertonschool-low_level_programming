f50329f362e16d02914210fc12dba0d31df32cc7
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-13 15:35:16 +0000

    0 to 9 sequence added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print a sequence from 0 to 9
+ *
+ * automated sequence to variable needed
+ * Return: no returns
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a);
+
+	}
+	_putchar('\n');
+}
