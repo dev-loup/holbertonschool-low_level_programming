7a8b55030554876a9de3fc111da047ac573287e1
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-13 20:34:03 +0000

    fixed holberton header

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -6,3 +9,6 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+int _putchar(char c);
+
+#endif /* HOLBERTON_H */
