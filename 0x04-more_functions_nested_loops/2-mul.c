ed9ebed0848c6118affcf5b3ea47f26d1805dfc4
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-13 15:24:43 +0000

    multiply function for two values in return

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * mul - multiply two integer values
+ * @a: input 1
+ * @b: input 2
+ *
+ * integers with ASCII code, return the result
+ * Return: returns the result
+*/
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+
+	return (c);
+}
