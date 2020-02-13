97c90fd653a94234f6962364bb98bf2caefbba41
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-13 20:49:14 +0000

    fixed numbers after 9

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,17 @@ void more_numbers(void)
 
 	for (b = 0; b < 10; b++)
 	{
-		for (a = 0; a < 15; a++)
+		for (a = 0; a < 14; a++)
 		{
-			_putchar(a + '0');
+			if (a > 9)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+			}
+			else
+				_putchar(a + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
