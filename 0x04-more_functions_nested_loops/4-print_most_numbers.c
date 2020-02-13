ec00777eca3c377655c1172ff907b904fea0e69b
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-13 15:44:39 +0000

    sequence 0 to 9 without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print a sequence from 0 to 9 no 2 or 4 allowed
+ *
+ * automated sequence to variable needed
+ * Return: no returns
+*/
+void print__most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a != 2 || a != 4)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
