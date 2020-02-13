2b69d04298e3c67300a79dc6ff47d0c0ad84f10c
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-13 16:03:18 +0000

    sequence 0 to 14 printed x10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print a sequence from 0 to 10 in loop 10 times
+ *
+ * automated sequence to variable needed
+ * Return: no returns
+*/
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 0; a < 10; a++)
+		{
+			_putchar(a);
+		}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
