75e5c0afe18dbde35d26d0967f030da26f2c30c9
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-14 02:37:20 +0000

    triangle fixed with diagonal matrix

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - based on diagonal matrix width = size -1
+ *@size: input to check
+ *
+ * Return: No return data
+ *
+ */
+void print_triangle(int size)
+{
+	int h;
+	int a;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (a = 0; a < size; a++)
+	{
+		for (h = size; h > a; h--)
+		{
+			_putchar(' ');
+		}
+		for (h = 0; h - 1 < a; h++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -11,5 +11,6 @@ void print_line(int n);
 void print_diagonal(int n);
 int _putchar(char c);
 void print_square(int size);
+void print_triangle(int size);
 
 #endif /* HOLBERTON_H */
