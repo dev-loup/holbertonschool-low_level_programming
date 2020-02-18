a6f90b93123c1fe0bbec3489845839596e0a8589
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-18 21:55:16 +0000

    8 int string for testing

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints integer values on array
+ * @a: the array pointer
+ * @n: number of positions to print
+ *
+ * Return: No returning
+ */
+void print_array(int *a, int n)
+{
+	int printer;
+
+	for (printer = 0; printer < n; printer++)
+	{
+		printf("%d", *(a + printer));
+		if (printer < n - 1)
+			printf(", ");
+	}
+	_putchar('\n');
+}
