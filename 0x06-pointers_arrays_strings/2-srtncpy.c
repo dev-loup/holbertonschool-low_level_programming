324323ee8e54461c569228f9f988cf94718fc075
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-20 19:53:04 +0000

    copy a string in n bytes length

---
+++ 0x06-pointers_arrays_strings/2-srtncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copy a string limited to n places
+ *
+ * @dest: string to concatenate
+ * @src: to contatenate string
+ * @n: byte limiter
+ * Return: array pointer dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && *(src + index) != '\0'; index++)
+	{
+		*(dest + index) = *(src + index);
+	}
+	if (*(src + index) == '\0')
+	{
+		index++;
+		*(dest + index) = '\0';
+	}
+
+	return (dest);
+}
