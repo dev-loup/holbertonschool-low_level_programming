1150e7bbd3b69968bb6bd8a24835456f29b4470b
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-20 17:57:19 +0000

    concatenate two strings, the second one is limited by n bytes

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -23,5 +23,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* HOLBERTON_H */
