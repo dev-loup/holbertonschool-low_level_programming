e32921fbeed79d14e43159e3a1831d25717feb4a
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-21 04:53:48 +0000

    print recursive numbers

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_number - print number
+ *
+ * @n: integer
+ * Return: nope!
+ */
+void print_number(int n)
+{
+	unsigned int integer = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		integer = -integer;
+	}
+	if ((integer / 10) > 0)
+		print_number(integer / 10);
+	_putchar((integer % 10 + 48);
+}
