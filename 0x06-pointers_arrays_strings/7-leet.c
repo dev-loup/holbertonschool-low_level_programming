7de4d37ef884ad7686e9eaef45965d1572226117
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-21 04:00:02 +0000

    leet decoder with loop an array comparison

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * leet - lower to upper case
+ * @a: string to upper
+ *
+ * Return: pointer a
+ */
+char *leet(char *a)
+{
+	int index;
+	int incheck;
+	int comp[] = "4433007711";
+	char leet[] = "AaEeOoTtLl";
+
+	for (index = 0; *(a + index) != '\0'; index++)
+	{
+		for (incheck = 0; *(leet + incheck) != '\0'; incheck++)
+			if (*(a + index) == *(leet + incheck))
+				*(a + index) = *(comp + incheck);
+	}
+	return (a);
+}
