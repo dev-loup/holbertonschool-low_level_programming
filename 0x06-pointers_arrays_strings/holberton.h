4f4b0b64ca48f4e3b2a88d934f62bec840092407
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-21 00:01:15 +0000

    fixed holberton.h file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+void reverse_array(int *a, int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -26,6 +27,6 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-void reverse_array(int *a; int n);
+
 
 #endif /* HOLBERTON_H */
