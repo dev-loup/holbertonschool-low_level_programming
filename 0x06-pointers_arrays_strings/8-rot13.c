64df24f86e4d154bb5bd9dab9c88f3814ab50519
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-21 04:15:36 +0000

    rot 13 with arrays

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encodes and decodes strings
+ * @a: string to upper
+ *
+ * Return: pointer a
+ */
+char *rot13(char *a)
+{
+	int index;
+	int incheck;
+	char comp[] = "NnOoPpQqRrSsTtUuVvXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char leet[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvXxYyZz";
+
+	for (index = 0; *(a + index) != '\0'; index++)
+	{
+		for (incheck = 0; *(leet + incheck) != '\0'; incheck++)
+		{
+			if (*(a + index) == *(leet + incheck))
+				*(a + index) = *(comp + incheck);
+		}
+	}
+	return (a);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -30,5 +30,6 @@ int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
 
 #endif /* HOLBERTON_H */
