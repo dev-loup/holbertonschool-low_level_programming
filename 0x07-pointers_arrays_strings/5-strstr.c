bc424b297490877d0199742ba93d310b3425edca
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-24 23:02:50 +0000

    str move with one loop

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strstr - returns the first substring coincidence.
+ * @haystack: string where you are looking for
+ * @needle: subtring to found
+ *
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index2;
+	int counter;
+
+	counter = 0;
+
+	for (index2 = 0; haystack[index2] != '\0'; index2++)
+	{
+		if (needle[counter] == haystack[index2])
+		{
+			if (needle[counter + 1] == '\0')
+				return (&haystack[index2 - counter]);
+			counter += 1;
+		}
+		else
+			counter = 0;
+
+	}
+	return (0);
+}
