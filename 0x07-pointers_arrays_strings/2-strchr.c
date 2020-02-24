41d960940de54e00f550198e689d36ecc116a099
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-24 18:32:40 +0000

    fixed return in null value when no character found

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,9 +11,9 @@ char *_strchr(char *s, char c)
 {
 	int index;
 
-	for (index = 0; *(s + index) != '\0'; index++)
+	for (index = 0; s[index] != '\0'; index++)
 	{
-		if (*(s + index) == c)
+		if (s[index] == c)
 			return (&s[index]);
 	}
 	return (00);
