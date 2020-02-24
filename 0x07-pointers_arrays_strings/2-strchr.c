0877936c6ecbe8393f4252e16e5d0b7b4b04e852
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-24 17:46:38 +0000

    fixed folder name

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates character in a string
+ *
+ * @s: the byte to write
+ * @c: the objective to locate
+ * Return: Returns pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; *(s + index) != c; index++)
+	{
+		if (*(s + index) == (c + 0))
+		{
+			
+		}
+		else
+			(taker + 0) = *(s + index);
+	}
+	return (taker);
+}
