a4e0e41d87f9188e8a9ac4306b0e8fad180b38c8
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-24 20:28:33 +0000

    string point break at first coincidence

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - returns a pointer to the first coincidence with a sequence
+ * @s: string to review
+ * @accept: the sequence
+ *
+ * Return: Returns null if there is no sequence
+ */
+unsigned int _strpbrk(char *s, char *accept)
+{
+	int index, index2, cutter;
+
+	cutter = 0;
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		for (index2 = 0; accept[index2] != '\0'; index2++)
+		{
+			if (s[index] == accept[index2])
+			{
+				return (&s[index]);
+				cutter += 1;
+			}
+		}
+	}
+	if (cutter > 0)
+		return (0);
+}
