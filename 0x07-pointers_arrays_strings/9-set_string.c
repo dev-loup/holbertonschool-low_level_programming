7585399fe58577a01bbab80cbaa341b9ad98746d
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-25 04:25:14 +0000

    set char to pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * set_string - set the value of a pointer to a char
+ * @s:
+ * @size: the size of the square
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
