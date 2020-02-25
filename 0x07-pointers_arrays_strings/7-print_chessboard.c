166336f8daa0782074e504298e09dfd48bdf5e42
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-25 00:47:10 +0000

    putchar chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints an non playable chessboard
+ *
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int ver;
+	int hor;
+
+	ver = 0;
+
+	for (hor = 0; hor != 8; hor++)
+	{
+		_putchar(a[ver][hor]);
+		if(hor == 7 && ver < 7)
+		{
+			hor = 0;
+			ver++;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
