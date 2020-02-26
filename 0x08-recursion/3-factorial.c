4c95230d53a7937470656a7d9f5044e9f076322b
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-26 23:28:19 +0000

    fixed return space after

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,12 +11,12 @@ int factorial(int n)
 	int counter;
 
 	if (n < 0)
-		return(-1);
+		return (-1);
 	if (n > 0)
 	{
 		counter = (factorial(n - 1));
 		return (counter * n);
 	}
 	else
-		return(1);
+		return (1);
 }
