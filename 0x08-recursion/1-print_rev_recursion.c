85eca444e0fb9a9c0a081b9b4d4967406b903c5d
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-26 19:41:11 +0000

    inverse string with recursive function

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints inverse strings
+ * @s: the string
+ * Return: no returns
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*(s + 1) != '\0')
+	{
+		s++;
+		putchar(*(s));
+		_print_rev_recursion(s);
+	}
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -3,5 +3,6 @@
 
 void _puts_recursion(char *s);
 int _putchar(char c);
+void print_rev_recursion(char *s);
 
 #endif /* HOLBERTON_H */
