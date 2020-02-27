b6bac502785f67bb8fd5573aa74b3509652b9807
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-27 00:50:20 +0000

    fixed undefined return at zero value

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,9 +13,11 @@ int _pow_recursion(int x, int y)
 
 	if (y < 0)
 		return (-1);
-	else if (y > 1)
+	else if (y > 0)
 	{
 		power = (_pow_recursion(x, (y - 1)) * x);
 		return (power);
 	}
+	else
+		return (1);
 }
