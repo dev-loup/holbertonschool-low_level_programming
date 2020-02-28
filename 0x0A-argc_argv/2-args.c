ada825425fffd74d01302fc3088219ab8b69b522
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-02-28 21:47:57 +0000

    fixed coding style

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the arguments
+ * @argc: counter to arguments input
+ * @argv: string of arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc __attribute__((unused)), char *argv)
+{
+	int counter;
+
+	for (counter = 0; argv[counter] != '\0'; counter++)
+	printf("%s\n", argv[counter]);
+	return (0);
+}
