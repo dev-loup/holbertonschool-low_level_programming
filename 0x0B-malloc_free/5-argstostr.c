ea33122a57562c4890c8d41b862e6a33a151aac9
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-04 03:32:08 +0000

    precheck

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,25 @@
+B#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * argstostr - concatenates arguments
+ * @av: the content of the array
+ * @ac: number of arguments
+ * Return: Nothing.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *arrcpy;
+	int counter;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	arrcpy = malloc(ac * (sizeof(char *)));
+	for (counter = 0; counter < ac; counter++)
+	{
+		for (scounter = 0; av[counter][scounter] != '\0'; scounter++)
+		{
+			arrcpy[counter] = av[counter][scounter];
+		}
+	}
+}
