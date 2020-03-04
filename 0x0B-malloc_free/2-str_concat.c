e75d628ba032467b9889283309ebac86246dcbd0
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-04 00:23:34 +0000

    fixed null conditions

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,12 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	else if (s1[0] == '\0' && s2[0] == '\0')
+		return (NULL);
 	else if (s1 == NULL)
 		s1 = "";
 	else if (s2 == NULL)
-	s2 = "";
+		s2 = "";
 	for (counter = 0; s1[counter] != '\0'; counter++)
 	{}
 	limiter = counter;
