92f852699c1eefeac4b8f9d47292519993acbb5e
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-04 02:44:22 +0000

    memory matrix cleaner

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,13 +4,14 @@
 /**
  * free_grid - free an array of arrays
  * @grid: the grid to wipe
- *
+ * @height: the size of array of arrays
  * Return: Nothing.
  */
 void free_grid(int **grid, int height)
 {
 	int counter;
-	for (counter = 0; counter < height ; counter++)
+
+	for (counter = 0; counter < height; counter++)
 	{
 		free(grid[counter]);
 	}
