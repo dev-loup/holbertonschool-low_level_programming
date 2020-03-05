bfde8683b0aebee1110af89a68f099fff909a069
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-05 19:13:53 +0000

    memory callocation

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _calloc - allocates memory for an array of nmemb elements of size bytes
+ * each and returns a pointer to the allocated memory.
+ * @nmemb: number of elements
+ * @size: the size of the element
+ *
+ * Return: Pointer to the element or null if nmemb, size or malloc null
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *callocer;
+	unsigned int allocer;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	callocer = malloc(nmemb * size);
+	if (callocer == NULL)
+		return (NULL);
+	for (allocer = 0; allocer < nmemb; allocer++)
+	{
+		callocer[allocer] = 0;
+	}
+	return (callocer);
+}
