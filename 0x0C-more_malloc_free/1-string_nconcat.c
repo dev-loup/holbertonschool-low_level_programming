2f9041511a294571886f2bc7b008e09f96bb3749
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-05 21:52:15 +0000

    cleaned code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,43 +12,34 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str_res;
-	unsigned int counter;
-	unsigned int switcher = NULL;
+	unsigned int counter, switcher = NULL;
 
-	for (counter = 0; s2[counter] != '\0'; counter++)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (switcher = 0; s2[switcher] != '\0'; switcher++)
 	{}
-	for (switcher = 0; s1[switcher] != '\0'; switcher++)
+	for (counter = 0; s1[counter] != '\0'; counter++)
 	{}
-	if (n < counter)
+	if (n < switcher)
 		str_res = malloc((n + 1) + counter);
 	else
-		str_res = malloc(counter + switcher);
+		str_res = malloc((counter + 1) + switcher);
 	switcher = 0;
 	if (str_res == NULL)
 		return (NULL);
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
 	for (counter = 0; str_res; counter++)
 	{
 		if (s1[counter] != '\0' && switcher == 0)
-		{
 			str_res[counter] = s1[counter];
-		}
 		else if (switcher == 0)
 		{
 			switcher = counter;
 			counter = 0;
 		}
 		if (switcher != 0 && counter < n)
-		{
 			str_res[switcher + counter] = s2[counter];
-		}
 		if (switcher != 0 && (counter == n || s2[counter] == '\0'))
 		{
 			str_res[switcher + counter] = '\0';
