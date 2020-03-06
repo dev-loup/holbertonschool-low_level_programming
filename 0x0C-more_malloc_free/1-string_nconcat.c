74cd69adea9fb623dd05c837a458fefefb637da7
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-06 03:20:52 +0000

    refab code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,39 +11,30 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int counter, counter2, limiter, index;
+	int counter1, counter2, upindex, index;
 	char *array;
 
 	if (s1 == NULL)
 		s1 = "";
 	else if (s2 == NULL)
 		s2 = "";
-	for (counter = 0; s1[counter] != '\0'; counter++)
+	for (counter1 = 0; s1[counter1] != '\0'; counter1++)
 	{}
 	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
 	{}
 	if (counter2 < n)
+		n = counter2;
+	array = malloc((counter1 + n + 1) * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for (index = 0; s1[index]; index++)
 	{
-		limiter = (counter + (counter2 + 1));
-		array = malloc(limiter * sizeof(char));
+		array[index] = s1[index];
 	}
-	else
+	for (upindex = 0; upindex < n; upindex++)
 	{
-		limiter = (counter + (n + 1));
-		array = malloc(limiter * sizeof(char));
+		array[index + upindex] = s2[upindex];
 	}
-	for (index = 0; index < limiter; index++)
-	{
-		if (index < counter)
-		{
-			array[index] = s1[index];
-		}
-		else if (index < (limiter - 1) && index >= counter)
-		{
-			array[index] = s2[index - counter];
-		}
-		else
-			array[index] = '\0';
-	}
+	array[index + upindex] = '\0';
 	return (array);
 }
