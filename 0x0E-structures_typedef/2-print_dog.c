52ea31536b875d1e78d055186c7a4d9a5ef45615
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-09 20:48:08 +0000

    null conditions added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,17 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (*d).name);
-		printf("Age: %f\n", (*d).age);
-		printf("Owner: %s\n", (*d).owner);
+		if ((*d).name != NULL)
+			printf("Name: %s\n", (*d).name);
+		else
+			printf("Name: (nil)");
+		if ((*d).name != NULL)
+			printf("Age: %f\n", (*d).age);
+		else
+			printf("Age: (nil)");
+		if ((*d).name != NULL)
+			printf("Owner: %s\n", (*d).owner);
+		else
+			printf("Owner: (nil)");
 	}
 }
