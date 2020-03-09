47760155aa399ccb4785d9dacd8f2e5f5f96c4ee
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-09 19:18:17 +0000

    dog struct description added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
-#ifndef DJANGO
-#define DJANGO
-struct dog {
+#ifndef DJANGO_H
+#define DJANGO_H
+
+/**
+ * struct dog - Django ID data
+ * @name: dog´s name
+ * @age: how old it is
+ * @owner: the dog's owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
