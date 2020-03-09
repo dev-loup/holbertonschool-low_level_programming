5e8b51b133ba408f17b5612ae59508c913a241e5
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-09 20:32:34 +0000

    fixed float

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (*d).name);
-		printf("Age: %.1f\n", (*d).age);
+		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
 	}
 }
