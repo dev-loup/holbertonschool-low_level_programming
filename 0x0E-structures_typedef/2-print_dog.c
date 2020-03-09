6ffdcc108715baa75620970745b0a5fe19cb0303
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-09 21:01:23 +0000

    fixed style

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,16 +9,15 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if ((*d).name != NULL)
-			printf("Name: %s\n", (*d).name);
-		else
-			printf("Name: (nil)");
-		printf("Age: %f\n", (*d).age);
-		if ((*d).name != NULL)
-			printf("Owner: %s\n", (*d).owner);
-		else
-			printf("Owner: (nil)");
-	}
+	if (d == NULL)
+		return;
+	if ((*d).name != NULL)
+		printf("Name: %s\n", (*d).name);
+	else
+		printf("Name: (nil)");
+	printf("Age: %f\n", (*d).age);
+	if ((*d).name != NULL)
+		printf("Owner: %s\n", (*d).owner);
+	else
+		printf("Owner: (nil)");
 }
