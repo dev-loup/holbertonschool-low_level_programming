067e83ca60e6ba713dc9988119c6dfe4daaf593a
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-09 19:54:11 +0000

    Django initializing struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - Django ID data
+ * @d: initializes dog
+ * @name: dog´s name
+ * @age: how old it is
+ * @owner: the dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
