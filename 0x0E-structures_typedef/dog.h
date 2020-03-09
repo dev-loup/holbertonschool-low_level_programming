20d0685f057ea77b0f26ecf892cd9126a8c781b6
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-09 20:02:18 +0000

    header updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DJANGO_H
 #define DJANGO_H
-
 /**
  * struct dog - Django ID data
  * @name: dog´s name
@@ -13,4 +12,5 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
