db39eabe86e74f7b5dcec01fbd751d2052543e1a
diego-940713 <60360505+diego-9407@users.noreply.github.com>
2020-03-11 17:22:35 +0000

    updated description in function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 /**
  * print_name - prints a name.
- *
+ * @name: received name to pass
+ * @f: pointer to function
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
