3aa1325375878b32addf02bdb3508f815ea4cace
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-19 23:29:44 +0000

    [add]: print struct info by nodes iteration
    the function prints data by iteratting in singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - print a full list.
+ * @h; struct list to print
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
