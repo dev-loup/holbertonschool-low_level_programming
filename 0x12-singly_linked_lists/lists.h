f300906f83ca80807c70959fc0bd4d1ee3a581f8
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-20 03:04:21 +0000

    [update]: moves libraries to header
    Libraries will be moved to header for better comprehension and less lines

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef SYNGLY_H
 #define SYNGLY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -20,5 +24,6 @@ typedef struct list_s
 
 int print_list(const list_t *h);
 int list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
