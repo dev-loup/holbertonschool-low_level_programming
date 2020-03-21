f49931caf0a8c3d057cca114d1d7bf4542ff8b83
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-21 19:17:41 +0000

    [update]: deleted if statement for adding days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,19 +16,11 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (!(year % 100 == 0))
 		{
-			if (month >= 2 && day >= 60)
-			{
-				day++;
-			}
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
 		else if (year % 400 == 0)
 		{
-			if (month >= 2 && day >= 60)
-			{
-				day++;
-			}
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
