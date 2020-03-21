9a3048bce26904e707ab7e297041f3c2533b91e7
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-21 22:38:44 +0000

    [update]: fix print numbers in gative under -100
    numbers under -100 were printed in ASCII values because hundreds and tens were taken as an unique value

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,43 +4,44 @@
  * print_to_98 - prints the absolute sequence to 98
  *
  * @n: defines the initial value
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void no returns
  */
 void print_to_98(int n)
 {
-	do {
-	if (n > 98)
+	while (n != 98)
 	{
-		if (n >= 100)
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-		n--;
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		n = (n * -1);
-		if (n >= 10)
-			_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		n--;
-		n = (n * -1);
-	}
-	else if (n >= 0)
-	{
-		if (n > 10)
+		if (n >= 98)
 		{
-			_putchar((n / 10) + '0');
+			if (n >= 100)
+				putchar((n / 100) + '0');
+			putchar(((n / 10) % 10) + '0');
+			putchar((n % 10) + '0');
+			n--;
 		}
-		_putchar((n % 10) + '0');
-		n++;
+		else if (n < 0)
+		{
+			putchar('-');
+			n = (n * -1);
+			if (n >= 100)
+				putchar((n / 100) + '0');
+			putchar(((n / 10) % 10) + '0');
+			putchar((n % 10) + '0');
+			n--;
+			n = (n * -1);
+		}
+		else if (n >= 0)
+		{
+			if (n > 10)
+			{
+				putchar((n / 10) + '0');
+			}
+			putchar((n % 10) + '0');
+			n++;
+		}
+		putchar(',');
+		putchar(' ');
 	}
-	_putchar(',');
-	_putchar(' ');
-	} while (n != 98);
-	_putchar((n / 10) + '0');
-	_putchar((n % 10) + '0');
-	_putchar('\n');
+	putchar((n / 10) + '0');
+	putchar((n % 10) + '0');
+	putchar('\n');
 }
