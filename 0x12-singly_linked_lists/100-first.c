337bb1fc6b7a811dd24d2563d22e11e6e7d90316
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-22 00:10:39 +0000

    [add]: first approach to attribute destructor and constructor
    Attribute constructor executes scripts declared in the attribute after library is loaded and destructor after ibrary is unloaded (tipically after exit program)

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * papaito - The hare beater.
+ * "ya vamo llegando a Penjamo!
+ * Return: void
+ */
+void __attribute__ ((constructor)) papaito()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
