0928da5f967a43b9b85012ede33a61000ddedae2
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-24 15:48:58 +0000

    [add]: printer for all elements in list_int type list
    the printer takes the listint_t struct and prints all the elements listed

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint- prints integer structed list
+ * @h: struct containing the integer
+ * prints all elements of a listint_t list
+ * return: number of elements found
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int counter = 0;
+
+	while (h)
+	{
+		if (h->n)
+			printf("%i\n", h->n);
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
