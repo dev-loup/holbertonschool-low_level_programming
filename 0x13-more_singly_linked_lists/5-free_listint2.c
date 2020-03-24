d692fdeb3a8430c33b7645bf0a0ae057dacebc6e
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-24 18:24:00 +0000

    [add]: free listint_t and sets the head to NULL
    Clean the list and sets the head pointer to NULL value

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - forward memory cleaning
+ * @head: linked list
+ * Return: None.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *end = *head;
+	if (*head)
+	{
+		while (*head)
+		{
+			end = *head;
+			*head = (*head)->next;
+			free(end);
+		}
+	}
+	head = NULL;
+}
