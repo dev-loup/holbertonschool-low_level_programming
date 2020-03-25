a565fe7fc22931d7c8ab4a28a0c780de6dc88f55
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-25 00:43:47 +0000

    [add]: get node with index positions
    this function uses an iterator to get the position and compares it with a desired index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns a node value at index position
+ * @head: struct containing the elements
+ * Return: pointer to indexed node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+	listint_t *cpy = head;
+
+	while (cpy != NULL)
+	{
+	  if (counter == index)
+		  return (cpy);
+	  counter++;
+	  cpy = cpy->next;
+	}
+		return (NULL);
+}
