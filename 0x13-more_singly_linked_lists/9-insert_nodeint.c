e0966089062d541d075cb7329017760d57ce6955
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-25 02:12:34 +0000

    [fix]: return wrong address, new is the iterator list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			cpy->n = n;
 			cpy->next = new->next;
 			new->next = cpy;
-			return (new);
+			return (cpy);
 		}
 		counter++;
 		new = new->next;
