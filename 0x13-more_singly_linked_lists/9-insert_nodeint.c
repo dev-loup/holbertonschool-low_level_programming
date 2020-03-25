d10c3ba8c1a288603515d77a46a2793f90ed6ceb
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-25 02:43:43 +0000

    [fix]: fixed idx position in iterator getting the wrong space

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (iter != NULL)
 	{
-		if (counter == idx)
+		if (counter == idx - 1)
 		{
 			cpy->n = n;
 			cpy->next = iter->next;
