e9f1e8b6f3d334deaa600e0694606e8132ae704e
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-03-27 03:48:09 +0000

    [add]: locate bit in a variable by using an index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * get_bit - get bit in position
+ * @n: binary string
+ * @index: index to check
+ * Return: bit in position
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int copy = n;
+
+	if (index > ((sizeof(unsigned long int) * 8) + 1)
+	copy >>= index;
+	if ((copy & 1) == 1)
+		return (1);
+	else
+		return (0);
+
+}
