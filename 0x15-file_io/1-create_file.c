de2bc06dcb39593a244317fce56679fe2458eb9e
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-04-01 01:24:25 +0000

    [add]: fixed if statement with misspelled pointer

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int rtno, rtnw, counter;
 
-	if (!filename || !text_context)
+	if (!filename || !text_content)
 	{
 		return (-1);
 	}
