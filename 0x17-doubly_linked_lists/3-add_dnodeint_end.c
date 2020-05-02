eb640ddc752ae334c6141c899943ae99a761f266
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-02 18:29:00 +0000

    [add]: Function for adding nodes at the end double linked lists
    this function iterates head to the end and puts a node at the end, or add a node to empty lists

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add the last node with pointer null in next.
+ * @head: address
+ * @n: int to add
+ * Return: new node pointer
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *end;
+
+	new = malloc(sizeof(dlistint_t));
+	end = *head;
+
+	if (new == NULL || n == 00)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		return (new);
+	}
+	while (end->next != NULL)
+		end = end->next;
+	end->next = new;
+	new->prev = end;
+	return (new);
+}
