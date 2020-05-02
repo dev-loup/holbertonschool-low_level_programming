9006b1f0f79b1dceb875dfe68d9fc8ad097ef625
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-02 17:32:44 +0000

    [add]: Return number of nodes in list
    prints all the nodes allocated in a double linked lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of listed elements
+ * @h: struct containing the elements
+ * Return: number of elements found
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int counter = 0;
+
+	while (h)
+	{
+		if (h->n)
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
