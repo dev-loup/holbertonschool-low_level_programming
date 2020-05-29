f9a615a1bf0c27032749c03c97ff67411e16a32f
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-29 17:58:19 -0500

    [add]: hash table create with size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
+	hash_table_t *table;
 
-    table = malloc(sizeof(hash_table_t));
-    if (table == NULL)
-    {
-        return (NULL);
-    }
-    table->size = size;
-    table->array = calloc(size, sizeof(hash_node_t *));
-    if (table->array == NULL)
-    {
-        free(table);
-        return (NULL);
-    }
-    return (table);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
 }
