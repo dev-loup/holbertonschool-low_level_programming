9b5e0dab3589fc756a4a5f5c3bd235a1d0295288
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-29 18:09:00 -0500

    [add] first release hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,7 +3,7 @@
 
 /**
  * hash_table_set - Adds a value to a hash table
- * 
+ *
  * @ht: hash table
  * @key: index parameter
  * @value: value to index
@@ -11,33 +11,33 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int val_index;
-    hash_node_t *runner;
+	unsigned long int val_index;
+	hash_node_t *runner;
 
-    val_index = key_index((const unsigned char *)key, ht->size);
-    runner = ht->array[val_index];
-    while (runner)
-    {
-        if (runner->value)
-        {
-            if (strcmp(runner->value, value) == 0)
-            {
-                return (0);
-            }
-        }
-        runner = runner->next;
-    }
-    
-    runner = malloc(sizeof(hash_node_t));
-    if (!runner)
-    {
-        free(runner);
-        return(0);
-    }
-    runner->key = key;
-    runner->value = value;
-    runner->next = ht->array[val_index];
-    ht->array[val_index] = runner;
-    
-    return (1);
+	val_index = key_index((const unsigned char *)key, ht->size);
+	runner = ht->array[val_index];
+	while (runner)
+	{
+		if (runner->value)
+		{
+			if (strcmp(runner->value, value) == 0)
+			{
+				return (0);
+			}
+		}
+		runner = runner->next;
+	}
+
+	runner = malloc(sizeof(hash_node_t));
+	if (!runner)
+	{
+		free(runner);
+		return (0);
+	}
+	runner->key = strdup(key);
+	runner->value = strdup(value);
+	runner->next = ht->array[val_index];
+	ht->array[val_index] = runner;
+
+	return (1);
 }
