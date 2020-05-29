9a14fd80987da9db3565043298b529eaf6acbee3
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-29 18:38:01 -0500

    [fix]: function description fixed

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,17 +1,17 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Implements djb2 Algorythm for sorting
+ * key_index - get an index from hash key
  * items in indexes
- * 
+ *
  * @key: value to index
  * @size: Size of the hash table
  * Return: hash index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index;
+	unsigned long int index;
 
-    index = hash_djb2(key) % size;
-    return (index);
+	index = hash_djb2(key) % size;
+	return (index);
 }
