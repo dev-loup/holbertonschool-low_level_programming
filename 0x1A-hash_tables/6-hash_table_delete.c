6b9d6d1c25a22ab5e9123afc42bf0a2dd4c82385
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-29 22:10:18 -0500

    [add]: free hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Erase a hash table
+ *
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *runner;
+    hash_node_t *temp;
+
+	if (!ht)
+	{
+		return;
+	}
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[index])
+		{
+			runner = ht->array[index];
+			while (runner)
+			{
+				temp = runner;
+				runner = runner->next;
+                free(temp->key);
+                free(temp->value);
+                free(temp);
+			}
+		}
+	}
+    free(ht->array);
+    free(ht);
+}
