4b357c71b7e87cb71737727c05b525a3bb77d60e
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-05-29 22:23:44 -0500

    [fix]: delete frees

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,15 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
-		free(table);
 		return (NULL);
 	}
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
-		free(table->array);
-		free(table);
 		return (NULL);
 	}
 	return (table);
